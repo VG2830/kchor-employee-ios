0ff8e89439747cfd609fee7834955fbda8ddd965
Ganesh <vgs170811@gmail.com>
2025-09-27 17:47:08 +0530

    payment gtway inte initiate

--- src/app/checkout-modal/checkout-modal.page.ts
+++ src/app/checkout-modal/checkout-modal.page.ts
@@ -18,23 +18,73 @@
 //     this.modalCtrl.dismiss();
 //   }
 // }
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NavParams, ModalController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { NavController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-checkout-modal',
+  standalone:true,
   templateUrl: './checkout-modal.page.html',
   styleUrls: ['./checkout-modal.page.scss'],
-  imports:[IonicModule]
+  imports:[IonicModule,ReactiveFormsModule,FormsModule,CommonModule]
 })
-export class CheckoutModalPage {
+export class CheckoutModalPage implements OnInit {
   plan: any;
-
-  constructor(private navParams: NavParams, private modalCtrl: ModalController) {
+   checkOutData:FormGroup;
+    user_id!: number;
+    number: string = '';
+  address: string = '';
+  name:string='';
+  email:string='';
+  constructor( private navParams: NavParams, 
+    private modalCtrl: ModalController, 
+    private fb: FormBuilder,
+    private apiService: ApiService,
+  ) 
+  {
     this.plan = this.navParams.get('plan');
-  }
 
+    this.checkOutData=this.fb.group({
+      name:['',Validators.required],
+      email:['',Validators.required],
+      mobile:['',Validators.required],
+      address:['',Validators.required]
+
+    })
+  }
+ ngOnInit(){
+   this.user_id = Number(localStorage.getItem('userId'));
+   this.plan = this.navParams.get('plan');
+   console.log(this.plan);
+  }
   closeModal() {
     this.modalCtrl.dismiss();
   }
+  onPayNow(){
+     if (this.checkOutData.invalid) {
+      this.checkOutData.markAllAsTouched();
+      return;
+    }
+    const formData = {
+      ...this.checkOutData.value,
+      user_id: this.user_id,
+    };
+   this.apiService.submitCheckoutData(formData).subscribe((res: any) => {
+        if (res.status === "success") {
+        // console.log(res);
+
+      var  planDuration=this.plan.duration;
+
+      console.log(planDuration);
+        this.apiService.orderCreate(planDuration).subscribe((res:any)=>{
+
+        })
+        }
+      });
+  }
+ 
 }
